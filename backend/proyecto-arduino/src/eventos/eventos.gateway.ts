5c841aa308d7efa5f341207f9284dfadf31bb470
Santiago <santiago.villegas@epn.edu.ec>
2022-09-04 18:56:45 -0500

    update/arduino

--- backend/proyecto-arduino/src/eventos/eventos.gateway.ts
+++ backend/proyecto-arduino/src/eventos/eventos.gateway.ts
@@ -1,2 +1,27 @@
+import {ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import {Server, Socket} from 'socket.io';
 
 
+
+@WebSocketGateway(
+    8080,
+    {
+        cors: {
+            origin: '*',
+        }
+    })
+export class EventosGateway {
+    @SubscribeMessage('saludar')
+    devolverHola(
+        @ConnectedSocket()
+            socket: Socket
+    ) {
+        socket.broadcast
+            .emit(
+                'escucharEventoSaludar',
+                {
+                    mensaje: 'Bienvenido '
+                });
+        return 'ok';
+    }
+}
