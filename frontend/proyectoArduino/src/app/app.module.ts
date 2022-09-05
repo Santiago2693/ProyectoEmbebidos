8eeb5a554986e9bb1d9f35842b925da6b6c81e2a
Santiago <santiago.villegas@epn.edu.ec>
2022-09-05 12:53:29 -0500

    update/ formnularios

--- frontend/proyectoArduino/src/app/app.module.ts
+++ frontend/proyectoArduino/src/app/app.module.ts
@@ -1,9 +1,17 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SocketIoModule } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RutaArduinoComponent } from './ruta-arduino/ruta-arduino.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
+import {LOCALE_ID} from '@angular/core';
+import {CommonModule, registerLocaleData} from "@angular/common";
+import localeEs from "@angular/common/locales/es";
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -13,13 +21,20 @@ import { RutaArduinoComponent } from './ruta-arduino/ruta-arduino.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     SocketIoModule.forRoot({
       url: 'ws://localhost:8080',
       options: {
       }
-    })
-  ],
-  providers: [],
+    }),
+    BrowserAnimationsModule,
+
+],
+  providers: [ {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+    {provide: LOCALE_ID, useValue: 'es'},
+
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/proyectoArduino/src/app/ruta-arduino/ruta-arduino.component.ts
+++ frontend/proyectoArduino/src/app/ruta-arduino/ruta-arduino.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Subscription} from 'rxjs';
 import {WebsocketsService} from 'src/servicios/websockets/websockets.service';
 
@@ -14,39 +15,97 @@ export class RutaArduinoComponent implements OnInit {
   r = 0;
   g = 0;
   b = 0;
+  color = `#ffdd2d`;
   mensaje = "";
+  fecha = new Date();
+  formGroup1?: FormGroup;
+  formGroup2?: FormGroup;
+
+
 
   constructor(
     public readonly websocketsService: WebsocketsService,
+    private readonly formBuilder: FormBuilder,
+
   ) {
   }
 
   ngOnInit(): void {
     this.logicaConexionArduino();
+    this.prepararFormularioMensaje();
+    this.prepararFormularioColor();
   }
+
+  prepararFormularioColor() {
+    this.formGroup2 = this.formBuilder
+      .group(
+        {
+          color: new FormControl(
+            {
+              value: this.color,
+              disabled: false
+            },
+            [
+              Validators.required, // min, max, minLength maxLength, email, pattern
+
+            ]
+          )
+        }
+      );
+  }
+
+  enviarMensaje() {
+    if (this.formGroup1) {
+      const mensaje1 = this.formGroup1.get('mensaje');
+      if (mensaje1) {
+        this.mensaje = mensaje1.value;
+        console.log(this.mensaje)
+      }
+    }
+  }
+
+  private prepararFormularioMensaje() {
+    this.formGroup1 = this.formBuilder
+      .group(
+        {
+          mensaje: new FormControl(
+            {
+              value: this.mensaje,
+              disabled: false
+            },
+            [
+              Validators.required, // min, max, minLength maxLength, email, pattern
+              Validators.maxLength(30),
+              Validators.pattern("[A-Za-z0-9]+"),
+            ]
+          )
+        }
+      );
+  }
+
 
   logicaConexionArduino() {
     this.desSuscribirse();
-    const respEscucharEventoSaludar=this.websocketsService.escucharEventoSaludar()
+    const respEscucharEventoSaludar = this.websocketsService.escucharEventoSaludar()
       .subscribe(
         {
-          next:(data)=>{
-            const informacion=data;
+          next: (data) => {
+            const informacion = data;
             console.log(data);
           },
-          error:(error)=>{
+          error: (error) => {
             console.error({error});
           }
         }
       );
-    const respEscucharEventoTemperaturaYHumedad=this.websocketsService.escucharEventoTemperaturaYHumedad()
+    const respEscucharEventoTemperaturaYHumedad = this.websocketsService.escucharEventoTemperaturaYHumedad()
       .subscribe(
         {
-          next:(data)=>{
-            const informacion=data;
+          next: (data) => {
+            const informacion = data;
             console.log(data);
           },
-          error:(error)=>{
+          error: (error) => {
             console.error({error});
           }
         }
@@ -65,7 +124,7 @@ export class RutaArduinoComponent implements OnInit {
 
   saludar() {
     this.websocketsService.ejecutarEventoSaludar();
-    this.websocketsService.ejecutarEventoEnviarRGB(125,125,125);
+    this.websocketsService.ejecutarEventoEnviarRGB(125, 125, 125);
     this.websocketsService.ejecutarEventoEnviarMensaje("Probando el saludo");
 
   }
